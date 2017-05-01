6b7e8830aa4d5d4d1b4dd3a5aa1dcbaac9a667c7
leohbatista <leonardohenriquebatista@gmail.com>
2017-05-01 16:05:40 -0300

    Adição do comando de criação das tabelas medico e medicamento. Criação do projeto Java da aplicação.

--- database/structure.sql
+++ database/structure.sql
@@ -1,2 +1,18 @@
 create database if not exists pronto_socorro;
 
+use pronto_socorro;
+
+CREATE TABLE IF NOT EXISTS medico (
+    CRM INT NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    especialidade VARCHAR(30) NOT NULL,
+    PRIMARY KEY (CRM)
+);
+
+CREATE TABLE IF NOT EXISTS medicamento (
+    id_medicamento INT NOT NULL AUTO_INCREMENT,
+    principio_ativo VARCHAR(80) NOT NULL,
+    dosagem VARCHAR(30) NOT NULL,
+    contra_indicacao BLOB NULL,
+    PRIMARY KEY (id_medicamento)
+);
